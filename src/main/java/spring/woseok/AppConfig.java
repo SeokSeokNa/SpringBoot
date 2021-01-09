7b91d7c28aefe950cd8fbb15685a268bbdd6a8d9
seok <nsk7640@naver.com>
2021-01-09 14:46:47 +0900

    새로운 할인정책 등장과 DIP , OCP 적용git add .

--- src/main/java/spring/woseok/AppConfig.java
+++ src/main/java/spring/woseok/AppConfig.java
@@ -9,7 +9,9 @@ import spring.woseok.member.MemberServiceImpl;
 import spring.woseok.member.MemoryMemberRepository;
 import spring.woseok.order.OrderService;
 import spring.woseok.order.OrderServiceImpl;
-
+//DIP : 프로그래머는 추상화에만 의존해야지 구체화까지 의존하지 말아야한다. (그래야 여기 AppConfig 같은 곳에서 쉽게 갈아 끼울수 있음 , 클래스 외부에서 넣어준다는 느낌)
+//OCP : 확장에는 열려있으나 변경에는 닫혀있어야 한다. ( 예를들어 새로운 할인정책 구현체를 만들면 원래 할인정책 구현체를 사용하던 모든 코드를 바꿔야한다..)
+//      (그러면 힘드니 애초에 구현체에 의존하지 않고 , 추상클래스에만 의존해서 코드변경없이 그대로 갈 수 있게 !!)
 //DIP 를 지키기 위해 AppConfig에서 구현체를 주입해준다 !!!
 //각 인터페이스들이 배우라면 배역을 정하는건 AppConfig
 // 이로서 각 인터페이스(배우)들은 상대역이 누구든간에 자기 할일만 시키면 되는 형태가 됨!!
@@ -29,7 +31,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
-    }
+        return new RateDiscountPolicy();
+    } //DIP , OCP 를 지키며 새로운 할인정책을 적용
 
 }
