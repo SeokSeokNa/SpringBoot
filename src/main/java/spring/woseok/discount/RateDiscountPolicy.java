557826d096ec158f55c1691e44213801eee6bdfd
wonseok <nsk7647@gmail.com>
2021-01-18 21:48:05 +0900

    Qualifier 어노테이션은 직접 만들어보기

--- src/main/java/spring/woseok/discount/RateDiscountPolicy.java
+++ src/main/java/spring/woseok/discount/RateDiscountPolicy.java
@@ -1,10 +1,12 @@
 package spring.woseok.discount;
 
 import org.springframework.stereotype.Component;
+import spring.woseok.annotation.MainDiscountPolicy;
 import spring.woseok.member.Grade;
 import spring.woseok.member.Member;
 
 @Component
+@MainDiscountPolicy //내가 직접만든 Qualifier 어노테이션 !!!
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
--- src/main/java/spring/woseok/order/OrderServiceImpl.java
+++ src/main/java/spring/woseok/order/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package spring.woseok.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import spring.woseok.annotation.MainDiscountPolicy;
 import spring.woseok.discount.DiscountPolicy;
 import spring.woseok.discount.FixDiscountPolicy;
 import spring.woseok.discount.RateDiscountPolicy;
@@ -12,7 +13,7 @@ import spring.woseok.member.MemoryMemberRepository;
 
 import java.util.Optional;
 @Component
-@RequiredArgsConstructor // final 키워드를 가진 필드를 파라미터로 가지는 생성자를 자동으로 만들어준다 !!!!
+//@RequiredArgsConstructor // final 키워드를 가진 필드를 파라미터로 가지는 생성자를 자동으로 만들어준다 !!!!
 public class OrderServiceImpl implements OrderService {
 
 
@@ -20,11 +21,11 @@ public class OrderServiceImpl implements OrderService {
     private  final DiscountPolicy discountPolicy;;
 
 
-//    @Autowired // 생성자가 하나 일 경우 생략 가능하다
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired // 생성자가 하나 일 경우 생략 가능하다
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
