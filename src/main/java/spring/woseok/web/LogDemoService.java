82085eae09ad9029abcb499cb63aa4b7bf79d44f
seok <nsk7640@naver.com>
2021-01-30 15:42:16 +0900

    request scope 사용해보기 (오류버전)

---
+++ src/main/java/spring/woseok/web/LogDemoService.java
@@ -0,0 +1,15 @@
+package spring.woseok.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.woseok.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
